e45191512530df1bac683e0ecda0233938a56073
Aysegoe <ayse.goekoez@hs-furtwangen.de>
2021-12-22 17:58:07 +0100

    Aufgabe6

---
+++ Aufgabe6/Client/client.js
@@ -0,0 +1,35 @@
+"use strict";
+var Client;
+(function (Client) {
+    console.log("Client läuft"); //Testausgabe
+    const url = "http://127.0.0.1:3000"; //URL Adresse, auf der der Server aktiv ist.
+    const path = "/greetings"; //Spezifischer Pfad, an den die Anfrage gehen soll.
+    const myForm = document.getElementById("myform"); //Referenzieren des Formulars
+    const sendButton = document.getElementById("send-button"); //Referenzieren des Send-Buttons
+    //EventListener für ein Click-Event aud den sendButton
+    sendButton.addEventListener("click", function (evt) {
+        evt.preventDefault(); //Vermeiden, dass Event wie gewöhlich ausgeführt wird
+        sendForm(); // sendForm-Funktion aufrufen.
+    });
+    console.log(myForm, sendButton); //Konsole-Log zum testen der HTML-Referenzen
+    /*
+    wenn wir das "await"-Stichwort in der Funktion verwenden müssen wir diese Funktion als "async" kennzeichnen.
+    Auserdem ist der Rückgabewert dann immer vom Typ: Promise;
+    */
+    async function sendForm() {
+        //Vorbereiten der Formulardaten zum Sende-Prozess
+        let formData = new FormData(myForm); //Formulardaten mit unserem Formular initialisieren.
+        let query = new URLSearchParams(formData); //Get-Parameter vorbereiten
+        let urlWithQuery = url + path + "?" + query.toString(); //Formatierung der URL zum Senden an den Server
+        /*
+        Die QueryURL besteht aus der url selbst,
+        aus dem gewünschten Pfad,
+        einem Fragezeichen um zu kennzeichnen das jetzt Getparameter angehengt werden
+        und der Get-Parametern (query.toString())
+        */
+        let response = await fetch(urlWithQuery); // Senden der Anfrage und warten auf Antwort
+        let responseText = await response.text(); // Warten auf den Response-Text;
+        console.log(responseText); // Ausgabe der Server-Antwort in der Konsole
+    }
+})(Client || (Client = {}));
+//# sourceMappingURL=client.js.map
---
+++ Aufgabe6/Server/server.ts
@@ -0,0 +1,41 @@
+import * as http from "http";
+
+namespace Server {
+  
+  const hostname: string = "127.0.0.1"; 
+  const port: number = 3000; 
+  const today: Date = new Date();
+  // tslint:disable-next-line: typedef
+  const year = today.getFullYear;
+
+  const server: http.Server = http.createServer(
+    (request: http.IncomingMessage, response: http.ServerResponse) => {
+
+      response.statusCode = 200; 
+
+      response.setHeader("Content-Type", "text/plain"); 
+      response.setHeader("Access-Control-Allow-Origin", "*"); 
+
+      let url: URL = new URL(request.url || "", `http://IncomingMessage.headers."127.0.0.1`); 
+
+      
+      switch (url.pathname) {
+        case "/": 
+          response.write("Server erreichbar");
+          break;
+        case "/convertDate":
+          let date: string = url.searchParams.get("date"); 
+          console.log(date); 
+          response.write(year); 
+          break;
+        default:
+          response.statusCode = 404; 
+      }
+      response.end(); 
+    }
+  );
+
+  server.listen(port, hostname, () => {
+    console.log(`Server running at http://"127.0.0.1":3000`);
+  });
+}
