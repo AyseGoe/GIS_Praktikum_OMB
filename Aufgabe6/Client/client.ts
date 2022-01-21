f4439a75f5725d36be7221cad786299fb2dc58f1
AyseGoe <92334647+AyseGoe@users.noreply.github.com>
2022-01-21 20:40:57 +0100

    Update client.ts

--- Aufgabe6/Client/client.ts
+++ Aufgabe6/Client/client.ts
@@ -1,41 +1,27 @@
-namespace Client {
+module Client {
+    const url: string = "http://localhost:3000/";
 
-  const url: string = "http://127.0.0.1:3000"; 
-  const path: string = "/convertDate"; 
-//  const anderepath: string ="/"  ;
+    const saveDatePath: string= "saveDate";
 
-  const myForm: HTMLFormElement = <HTMLFormElement>document.getElementById("myform"); 
-  const sendButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("send-button"); 
-  const convertButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("convert-button"); 
-  const display: HTMLDivElement = <HTMLDivElement>document.getElementById("display");
-  const date: HTMLInputElement = <HTMLInputElement> document.getElementById("date");
+    const form: HTMLFormElement = <HTMLFormElement>document.getElementById("myform");
+    const sendDateButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("send-button");
+    const display: HTMLDivElement = <HTMLDivElement>document.getElementById("display");
 
-  sendButton.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    sendForm(); 
-});
+    sendDateButton.addEventListener("click", (evt) => {
+        evt.preventDefault();
+        sendForm(saveDatePath);
+    });
 
-  async function GetDate(url: RequestInfo): Promise<string> {
-    let response: Response = await fetch(url); 
-    let responseText: string = await response.text(); 
+    async function sendForm(path: String): Promise<void> {
+        let formData: FormData = new FormData(form);
+        let query: URLSearchParams = new URLSearchParams(<any>formData);
+        let urlWithQuery: string = url + path + "?" + query.toString();
 
-    return responseText;
-  }
+        let response: Response = await fetch(urlWithQuery);
+        let text: string = await response.text();
 
-  async function sendForm(): Promise<void> {
-    let inputValue: string = JSON.stringify(date.value);
-    let serverAntwort: string = await GetDate(url + path + `?responseText =${inputValue}`);
-    sendForm2(serverAntwort);
-    //datezumServer
-    //  let formData: FormData = new FormData(myForm); 
-    //  let query: URLSearchParams = new URLSearchParams(<any>formData); 
-    //  let urlWithQuery: string = url + path + "?" + query.toString(); 
-  } 
-  function sendForm2(ServerResponse: string): void {
-    let neudate: HTMLElement = document.createElement("p");
-    neudate.className = "ServerResponse";
-    neudate.textContent = ServerResponse;
-    display.appendChild(neudate);
-  }
+        display.textContent = text;
+    }
 }
-const convertDatePath: string = "";
+
+const saveDatePath: string = "";
