51629481273e797b10f78b99319d30d889b680ff
AyseGoe <92334647+AyseGoe@users.noreply.github.com>
2022-01-21 20:43:58 +0100

    Update server.ts

--- Aufgabe6/Server/server.ts
+++ Aufgabe6/Server/server.ts
@@ -1,41 +1,8 @@
 import * as http from "http";
-namespace Server {  
+module Server {
   const hostname: string = "127.0.0.1"; 
-  const port: number = 3000; 
+  const port: number = 3000;
 
-  const server: http.Server = http.createServer(
-    (request: http.IncomingMessage, response: http.ServerResponse) => {
-      response.statusCode = 200; 
-      response.setHeader("Content-Type", "text/plain"); 
-      response.setHeader("Access-Control-Allow-Origin", "*"); 
-      let url: URL = new URL(request.url || "", `http://${request.headers.host}`); 
-      routing(response, url);
-      response.end();
-    }
-  );
-
-  function routing(response: http.ServerResponse, url: URL): void {
-    let dateString: string;
-    switch (url.pathname) {
-        case "/": 
-         // let today: string = url.searchParams.get("date"); 
-          response.write("Server erreichbar");
-          break;
-        case "/convertDate":
-          dateString = convertDate(url.searchParams.get("date") || ""); 
-          response.end(dateString); 
-          break;
-         // case "/convertDate2":
-         // let currentDate: Date = new Date();
-        //    console.log(date); 
-       //   response.write(date); 
-       //   break;
-        default:
-          response.statusCode = 404; 
-      }
-    response.end(); 
-  }
-  
   const monthStrings = [
     "January",
     "February",
@@ -51,6 +18,35 @@ namespace Server {
     "December",
   ];
 
+  const server: http.Server = http.createServer(
+    (request: http.IncomingMessage, response: http.ServerResponse) => {
+      response.statusCode = 200;
+      response.setHeader("Content-Type", "text/plain");
+      response.setHeader("Access-Control-Allow-Origin", "*"); 
+      let url: URL = new URL(
+        request.url || "",
+        `http://${request.headers.host}`
+      );
+      routing(response, url);
+      response.end();
+    }
+  );
+
+  function routing(response: http.ServerResponse, url: URL): void {
+    let dateString: string;
+    switch (url.pathname) {
+      case "/":
+        response.write("Server erreichbar");
+        break;
+      case "/saveDate":
+        dateString = convertDate(url.searchParams.get("date"));
+        response.write(dateString);
+        break;
+      default:
+        response.statusCode = 404;
+    }
+  }
+
   function convertDate(dateString: string): string {
     let date: Date = new Date(dateString);
     return `Month: ${monthStrings[date.getMonth()]
@@ -60,4 +56,4 @@ namespace Server {
   server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
   });
-  }
+}
